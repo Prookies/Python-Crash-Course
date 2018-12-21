92570d49e2d913d202c6880f7a11286cbf9725df
Xuhua <13631784452@163.com>
2018-12-21 21:01:08 +0800

    第四章全部例程

---
+++ unit4/practice4_13.py
@@ -0,0 +1,6 @@
+buffet_foods=('beef','coke','lamb','ice cream','milk')
+for food in buffet_foods:
+    print(food)
+buffet_foods=('beef','bread','lamb','hamburger','milk')
+for food in buffet_foods:
+    print(food)
